cff7a7db89c453c5665bb0843b3a1b178921be41
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-10-09 20:50:52 -0300

    Refactor (WIP).

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,11 @@
 ﻿import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
-
-import { List } from '../_models';
-import { ListService } from '../_services';
 
 @Component({templateUrl: 'home.component.html'})
 export class HomeComponent implements OnInit {
-    lists: List[];
-    
-    constructor(private listService: ListService) {}
+
+    constructor() {}
 
     ngOnInit() {
-        this.listService.getAll().pipe(first()).subscribe(page => { 
-            this.lists = page.content; 
-            console.log(page);
-        });
+
     }
 }
