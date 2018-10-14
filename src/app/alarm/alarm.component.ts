8495cefe8e64dca72413636524eb3af428a7bb67
Fernando <wzfernandochen@gmail.com>
2018-10-14 06:54:59 -0300

    Event actions dialog.

--- src/app/alarm/alarm.component.ts
+++ src/app/alarm/alarm.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { AlarmSummary, Event } from 'src/app/_models';
 import { AlarmService } from 'src/app/_services/alarm.service';
 import { first } from 'rxjs/internal/operators/first';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { EventDialogComponent } from 'src/app/list/event-dialog/event-dialog.component';
+import { SearchElementDialogComponent } from 'src/app/reusable/search-element-dialog/search-element-dialog.component';
+import { ListService } from 'src/app/_services';
 
 @Component({
     selector: 'app-alarm',
@@ -16,9 +20,13 @@ export class AlarmComponent implements OnInit {
 
     private loadingAlarmIds: number[] = [];
 
+    eventColumns = ['name', 'start', 'status', 'action'];
+
     constructor(
         private spinner: NgxSpinnerService,
-        private alarmService: AlarmService) { }
+        public dialog: MatDialog,
+        private alarmService: AlarmService,
+        private listService: ListService) { }
 
     ngOnInit() {
         this.spinner.show();
@@ -51,5 +59,31 @@ export class AlarmComponent implements OnInit {
 
     isLoading(alarm: AlarmSummary) {
         return this.loadingAlarmIds.includes(alarm.id);
+    }
+
+    openEventDialog(index, event): void {
+        const dialogRef = this.dialog.open(EventDialogComponent, {
+            width: '500px',
+            data: { event: event }
+        });
+    }
+
+    openAddEventDialog(event): void {
+        const dialogRef = this.dialog.open(SearchElementDialogComponent, {
+            width: '500px',
+            data: {
+                event: event
+            }
+        });
+        dialogRef.afterClosed().subscribe(list => {
+            if (list) {
+                this.spinner.show();
+                this.listService.addEvent(list, event)
+                    .pipe(first())
+                    .subscribe(response => {
+                        this.spinner.hide();
+                    });
+            }
+        });
     }
 }
