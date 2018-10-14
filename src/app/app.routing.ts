bcb079371fcaf4887153e4167746d82712f0e3a0
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-10-14 05:43:00 -0300

    registration feature added.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,17 @@
 ﻿import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { AuthGuard } from './_guards';
 import { SearchComponent } from 'src/app/search/search.component';
 import { ListComponent } from './list/list.component';
 import { AccountComponent } from 'src/app/account/account.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'lists', component: ListComponent, canActivate: [AuthGuard] },
+    { path: 'register', component: RegistrationComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'lists', component: ListComponent, canActivate: [AuthGuard] },
     { path: 'search', component: SearchComponent, canActivate: [AuthGuard] },
     { path: 'account', component: AccountComponent, canActivate: [AuthGuard] },
     // otherwise redirect to home
