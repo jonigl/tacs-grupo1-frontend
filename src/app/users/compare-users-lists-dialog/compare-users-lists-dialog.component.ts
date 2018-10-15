82a400637c2f5bca0d060a506956f6c4e3390722
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-10-15 01:27:33 -0300

    compare user lists feature working.

--- src/app/users/compare-users-lists-dialog/compare-users-lists-dialog.component.ts
+++ src/app/users/compare-users-lists-dialog/compare-users-lists-dialog.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA, MatSelectionList, MatListOption } from '@angular/material';
-import { List } from 'src/app/_models';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatTableDataSource, MatTable, MatSnackBar, MatSort } from '@angular/material';
+import { List, Event } from 'src/app/_models';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ListService } from 'src/app/_services';
@@ -12,20 +12,28 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./compare-users-lists-dialog.component.css']
 })
 export class CompareUsersListsDialogComponent implements OnInit {
+  @ViewChild('eventTable') eventTable: MatTable<Element>;
+  @ViewChild(MatSort) sort: MatSort;
   listUser1: List;
   listUser2: List;
+  events: Event[];
+  dataSourceEvents: MatTableDataSource<Event>;
+  displayedEventColumns = ['name', 'start', 'status'];
 
   constructor(
     public dialogRef: MatDialogRef<CompareUsersListsDialogComponent>,
     private _formBuilder: FormBuilder,
     private spinner: NgxSpinnerService,
     private listService: ListService,
-    @Inject(MAT_DIALOG_DATA) public data: object) { }
+    private snackbar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
   ngOnInit() {
+    this.hasLists(this.data.user1);
+    this.hasLists(this.data.user2);
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
@@ -34,14 +42,28 @@ export class CompareUsersListsDialogComponent implements OnInit {
     });
   }
 
+  hasLists(user: any) {
+    if (user.lists.length === 0) {
+      this.snackbar.open(`User: ${user.details.username} don't have lists`, '', { duration: 3000 });
+      this.dialogRef.close();
+    }
+  }
+
   onNoClick(): void {
     this.dialogRef.close(false);
   }
 
   compareLists() {
     this.spinner.show();
-    this.listService.compare(this.listUser1, this.listUser2).pipe(first()).subscribe(response => {
+    this.listService.compare(this.listUser1, this.listUser2).pipe(first()).subscribe(page => {
       this.spinner.hide();
+      this.events = page.content;
+      this.dataSourceEvents = new MatTableDataSource(this.events);
+      if (this.events.length > 0) {
+        this.dataSourceEvents.sort = this.sort;
+      } else {
+        this.snackbar.open('No lists found', '', { duration: 3000 });
+      }
     });
   }
 
